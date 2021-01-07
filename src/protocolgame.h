8a71997055ef037106035ae4f4711815e4e7b990
Sarah Wesker <sarahelizabetwesker@gmail.com>
2021-01-07 01:36:01 -0400

    Don't delete players on death

--- src/protocolgame.h
+++ src/protocolgame.h
@@ -68,6 +68,8 @@ class ProtocolGame final : public Protocol
 
 		void login(const std::string& name, uint32_t accountId, OperatingSystem_t operatingSystem);
 		void logout(bool displayEffect, bool forced);
+		void spawn();
+		void despawn();
 
 		uint16_t getVersion() const {
 			return version;
